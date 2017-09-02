c4e01c1543c1a438c5a3b2811098bf3233b1c029
quoctrungdh <quoctrungdh@gmail.com>
2017-09-02 18:05:16 +0700

    feat(config): replace css by scss loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -152,7 +152,12 @@ module.exports = {
               // directory for faster rebuilds.
               cacheDirectory: true,
             },
-          },
+					},
+					{
+						test: /\.scss$/,
+						include: paths.appSrc,
+						loaders: [require.resolve('style-loader'), require.resolve('css-loader'), require.resolve('sass-loader')]
+					},
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
           // "style" loader turns CSS into JS modules that inject <style> tags.
